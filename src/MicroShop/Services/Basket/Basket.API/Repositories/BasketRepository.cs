2061e248a36dad0cc451dbb224c9f2da19950544
Diogo Kobbi <diogokobbi@gmail.com>
2022-01-01 14:02:43 -0300

    Criando microserviço com acesso ao Redis

---
+++ src/MicroShop/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,35 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _cache;
+        public BasketRepository(IDistributedCache cache)
+        {
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+        }
+        async Task IBasketRepository.DeleteBasket(string username)
+        {
+            await _cache.RemoveAsync(username);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username)
+        {
+            var basket = await _cache.GetStringAsync(username);
+            if (String.IsNullOrEmpty(basket))
+                return null;
+            else
+                return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _cache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
